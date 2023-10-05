2deba7fb3f326f483d1d9104dd522ef1df1f5c33
Stiven <stivenmol065@gmail.com>
2023-10-05 11:12:15 -0700

    changes when place shape
    shape blocks change to floor type when placed

--- scripts/BlockFactory.gd
+++ scripts/BlockFactory.gd
@@ -1,7 +1,7 @@
 extends Node
 class_name BlockFactory
 
-var block_atlas_coords = PackedVector2Array([
+var placed_blocks_atlas_coords = PackedVector2Array([
 	Vector2(0, 0), # I
 	Vector2(2, 0), # J
 	Vector2(5, 0), # L
@@ -21,12 +21,24 @@ var shapes = [
 	PackedVector2Array([Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(2, 1)]) # Z
 ]
 
+var blocks_atlas_coords = [
+	PackedVector2Array([Vector2(0, 13), Vector2(0, 14), Vector2(0, 15), Vector2(0, 16)]), # I
+	PackedVector2Array([Vector2(0, 9), Vector2(0, 10), Vector2(1, 10), Vector2(2, 10)]), # J
+	PackedVector2Array([Vector2(2, 3), Vector2(0, 4), Vector2(1, 4), Vector2(2, 4)]), # L
+	PackedVector2Array([Vector2(0, 5), Vector2(1, 5), Vector2(0, 6), Vector2(1, 6)]), # O
+	PackedVector2Array([Vector2(1, 11), Vector2(2, 11), Vector2(0, 12), Vector2(1, 12)]), # S
+	PackedVector2Array([Vector2(1, 7), Vector2(0, 8), Vector2(1, 8), Vector2(2, 8)]), # T
+	PackedVector2Array([Vector2(0, 1), Vector2(1, 1), Vector2(1, 2), Vector2(2, 2)]) # Z
+]
+
 var shape
 
 func get_shape():
-	#return shapes_scene[0].instantiate()
 	shape = shapes.pick_random()
 	return shape
 
-func get_block_atlas_coord():
-	return block_atlas_coords[shapes.find(shape)]
+func get_block_atlas_coords():
+	return blocks_atlas_coords[shapes.find(shape)]
+
+func get_placed_block_atlas_coords():
+	return placed_blocks_atlas_coords[shapes.find(shape)]
--- scripts/Board.gd
+++ scripts/Board.gd
@@ -7,7 +7,7 @@ class_name Board
 
 var block_fact = BlockFactory.new()
 var shape: PackedVector2Array
-var block_atlas_coord: Vector2
+var block_atlas_coords: PackedVector2Array
 var shape_pos: PackedVector2Array
 var tile_num_width
 var tile_map_scale
@@ -19,81 +19,92 @@ enum Block_Type {
 	FLOOR = 2,
 	BLOCK = 3
 }
-var board_block_types
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tile_num_width = tile_map.get_used_rect().size.x
 	tile_map_scale = tile_map.scale
 	middle_tile = tile_num_width / 2 - 1
-	blank_tile_pos = Vector2(9, 3)
+	blank_tile_pos = Vector2(9, 3) # posicion de celda vacia en tilemap
 	
 	spawn_shape()
 
 func spawn_shape():
-	shape_pos.clear()
+	shape_pos.clear() # limpia la posicion de la figura en el tablero
 	shape = block_fact.get_shape()
-	block_atlas_coord = block_fact.get_block_atlas_coord()
+	block_atlas_coords = block_fact.get_block_atlas_coords()
 	
+	# pinta en tilemap cada celda de la figura
 	for n in shape.size():
+		# guarda la posicion de la figura en el tablero para el movimiento
 		shape_pos.append(shape[n] + Vector2(middle_tile, 1))
-		tile_map.set_cell(0, shape_pos[n], 4, block_atlas_coord)
+		tile_map.set_cell(0, shape_pos[n], 4, block_atlas_coords[n])
 	
-	timer.start(1)
+	# inicia un temporizador con 1 segundo
+	timer.start(shape_delay)
 
 func _physics_process(_delta):
 	if !shape:
 		return
 	
+	# al acabar el tiempo, mueve la figura una posicion abajo
 	if timer.time_left == 0:
 		move(Vector2.DOWN)
-		timer.start(1)
+		timer.start(shape_delay)
 
 func move(direction):
 	var new_pos: PackedVector2Array
 	var new_tile_data: TileData
 	
-	for n in shape_pos.size():
-		new_pos.append(shape_pos[n] + direction)
+	# itera cada bloque de la figura para verificar su siguiente posicion
+	for block_pos in shape_pos.size():
+		new_pos.append(shape_pos[block_pos] + direction)
 		
-		new_tile_data = tile_map.get_cell_tile_data(0, new_pos[n])
+		new_tile_data = tile_map.get_cell_tile_data(0, new_pos[block_pos])
 		
 		match new_tile_data.get_custom_data_by_layer_id(0):
 			Block_Type.WALL:
-				return
+				return true
 			Block_Type.FLOOR:
+				set_shape_as_floor()
 				spawn_shape()
-				return
+				return false
 	
+	# una vez verificado que se puede mover a la siguiente posicion
 	for n in shape_pos.size():
-		new_tile_data = tile_map.get_cell_tile_data(0, new_pos[n])
-		
-		if new_tile_data.get_custom_data_by_layer_id(0) != Block_Type.BLOCK:
-			continue
-		
-		if shape_pos.has(new_pos[n]):
-			continue
-		
-		spawn_shape()
-		return
-	
-	for n in shape_pos.size():
+		# se limpian las celdas actuales de la figura
 		set_tile(shape_pos[n], blank_tile_pos)
 	
+	# y se vuelve a pinar la figura pero en su nueva posicion
 	for n in new_pos.size():
-		new_tile_data = tile_map.get_cell_tile_data(0, new_pos[n])
-		set_tile(new_pos[n], block_atlas_coord)
+		set_tile(new_pos[n], block_atlas_coords[n])
 		shape_pos[n] = new_pos[n]
+	
+	return true
 
 func set_tile(tile_pos, atlas_pos):
 	tile_map.set_cell(0, tile_pos, 4, atlas_pos)
 
+func set_shape_as_floor():
+	var pos: PackedVector2Array
+	var tile_data: TileData
+	var placed_block = block_fact.get_placed_block_atlas_coords()
+	
+	# recorre cada bloque de la figura
+	for block_pos in shape_pos:
+		# asigna a la posicion actual del bloque, otro bloque pero de tipo FLOOR
+		tile_map.set_cell(0, block_pos, 4, placed_block)
+
 func _unhandled_input(_event):
-	if Input.is_action_just_pressed("left"):
+	if Input.is_action_pressed("left"):
 		move(Vector2.LEFT)
 	
-	if Input.is_action_just_pressed("right"):
+	if Input.is_action_pressed("right"):
 		move(Vector2.RIGHT)
 	
 	if Input.is_action_pressed("down"):
 		move(Vector2.DOWN)
+	
+	if Input.is_action_just_pressed("place"):
+		while move(Vector2.DOWN):
+			continue
